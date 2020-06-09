d1e4c752047906ac9e79e2eea45bfbdca0772b91
Michał Fluder <fludermichal@gmail.com>
2020-06-10 01:10:39 +0200

    add file with create table

---
+++ sql_create_database.sql
@@ -0,0 +1,74 @@
+DROP DATABASE IF EXISTS spedycja;
+CREATE DATABASE IF NOT EXISTS spedycja;
+USE spedycja;
+
+SELECT 'CREATING DATABASE STRUCTURE' as 'INFO';
+
+DROP TABLE IF EXISTS pracownicy,
+                     flota,
+                     klienci,
+                     zlecenia, 
+                     pensje,
+					 ceny_paliwa, 
+					 oplaty;
+
+
+CREATE TABLE pracownicy (
+    id_pracownika      INT             NOT NULL,
+    Imie               VARCHAR(30)     NOT NULL,
+    Nazwisko           VARCHAR(50)     NOT NULL,
+    PRIMARY KEY (id_pracownika)
+);
+
+CREATE TABLE flota (
+    id_samochodu     INT        NOT NULL,
+    waga_samochodu   DOUBLE     NOT NULL,
+    przebieg         DOUBLE     NOT NULL,
+    spalanie         DOUBLE     NOT NULL,
+    max_zaladunek    DOUBLE     NOT NULL,
+    PRIMARY KEY (id_samochodu)
+);
+
+
+CREATE TABLE klienci (
+   id_klienta       INT             NOT NULL,
+   Imie             VARCHAR(30)     NOT NULL,
+   Nazwisko         VARCHAR(50)     NOT NULL,
+   e_mail           VARCHAR(80)     NOT NULL,
+   telefon          VARCHAR(15)     NOT NULL,
+   miejscowosc      VARCHAR(50)     NOT NULL,
+   PRIMARY KEY (id_klienta)
+); 
+
+CREATE TABLE zlecenia (
+    id_zlecenia      INT             NOT NULL,
+    id_pracownika    INT             NOT NULL,
+    id_samochodu     INT             NOT NULL,
+    id_klienta       INT             NOT NULL,
+    FOREIGN KEY (id_pracownika)  REFERENCES   pracownicy   (id_pracownika)  ON DELETE CASCADE,
+    FOREIGN KEY (id_samochodu)   REFERENCES   flota   (id_samochodu)   ON DELETE CASCADE,
+    FOREIGN KEY (id_klienta)     REFERENCES   klienci   (id_klienta)     ON DELETE CASCADE,
+    PRIMARY KEY (id_zlecenia)
+);
+
+CREATE TABLE pensje (
+    id_pracownika    INT           NOT NULL,
+    pensja           VARCHAR(50)   NOT NULL,
+    od_daty          DATE          NOT NULL,
+    do_daty          DATE          NOT NULL,
+    FOREIGN KEY (id_pracownika)  REFERENCES   pracownicy   (id_pracownika)  ON DELETE CASCADE,
+    PRIMARY KEY (id_pracownika)
+);
+
+CREATE TABLE ceny_paliwa (
+    cena      DOUBLE     NOT NULL,
+    data      DATE       NOT NULL,
+    PRIMARY KEY (data)
+); 
+
+CREATE TABLE oplaty (
+    rodzaj_oplaty      VARCHAR(30)     NOT NULL,
+    kwota_transakcji   DOUBLE          NOT NULL,
+    data               DATE            NOT NULL,
+    PRIMARY KEY (data)
+); 
